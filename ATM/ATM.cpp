cf1e9f32891c69d04ab02f9c70020c16a5b93aad
Victor 'Macrev <victorcruzrodrigues2003@gmail.com>
2022-03-10 16:18:56 -0300

    Create ATM

---
+++ ATM/ATM.cpp
@@ -0,0 +1,47 @@
+#include <iostream>
+using namespace std;
+
+void showMenu() {
+    cout << "***********MENU***********" << endl;
+    cout << "1. Check Balance" << endl;
+    cout << "2. Deposit" << endl;
+    cout << "3. Withdraw" << endl;
+    cout << "**************************" << endl;
+}
+
+
+
+int main()
+{
+    //check balance, deposit, withdraw, show menu
+
+    int option;
+    double balance = 500;
+
+    do{
+        showMenu();
+        cout << "Option: ";
+        cin >> option;
+
+        switch (option) {
+        case 1:cout << "Balance is: " << balance << " $" << endl; break;
+
+        case 2:cout << "Deposit amout: ";
+            double depositAmout;
+            cin >> depositAmout;
+            balance += depositAmout;
+            break;
+
+        case 3:cout << "Withdraw amout: ";
+            double withdrawAmout;
+            cin >> withdrawAmout;
+            if (withdrawAmout <= balance)
+                balance -= withdrawAmout;
+            else
+                cout << "Not enough money" << endl;
+            break;
+        }
+    } while (option!=4);
+     
+    system("pause>0");
+}
